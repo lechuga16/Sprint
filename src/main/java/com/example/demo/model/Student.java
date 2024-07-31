bab7261fc920afde68ede267207c096a74452415
Lechuga <israel.la@hotmail.com>
2024-07-31 17:19:15 -0400

    Delete getmame adn setname
    - detalles

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -14,11 +14,4 @@ public class Student {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
